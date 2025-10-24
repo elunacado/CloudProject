f2cf371d8bad62475533f97c512912a06c2fcf27
Ethan Luna <ethanlunacado@gmail.com>
2025-10-24 17:51:46 +1300

    feat added the base for the project

---
+++ backend/src/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from backend!" });
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Backend running on port ${PORT}`));
