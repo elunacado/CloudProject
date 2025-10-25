eb5314032bc49bf9c8dd37581a097f01782adc5b
Ethan <ethanlunacado@gmail.com>
2025-10-25 21:41:41 +1300

    added the sidebar to the lister view

--- frontend/src/views/Dashboard/DashboardLister.jsx
+++ frontend/src/views/Dashboard/DashboardLister.jsx
@@ -1,8 +1,17 @@
+import Sidebar from "../../components/Sidebar/Sidebar";
+
+
 export default function DashboardLister() {
+  const listMenu = [" 👤 Profile", "📅 Room Management", "⏻ Logout"];
+
   return (
-    <div>
-      <h1>Dashboard Lister</h1>
-      
+    <div style={{ display: "flex", height: "100vh" }}>
+      <Sidebar items={listMenu} />
+
+      <main style={{ flex: 1, padding: "40px" }}>
+        <h1>Dashboard Lister</h1>
+        <p>Welcome to the lister's dashboard.</p>
+      </main>
     </div>
   );
 }
